210a1dcbf6f4b2487ac8be0ced7985aed55f0776
giocerz <grce2000@gmail.com>
2025-07-02 15:52:04 -0500

    fix: initialize _prev_view as a list and update method docstrings for clarity

--- src/providers/SaveProvider.py
+++ src/providers/SaveProvider.py
@@ -4,16 +4,18 @@ class SaveProvider:
     def __new__(cls, *args, **kwargs):
         if cls._instance is None:
             cls._instance = super().__new__(cls)
-            cls._instance._prev_view = None  # Initially no value
+            cls._instance._prev_view = []  # Initially no value
             #cls._instance.filter = MovingAverageFilter(num=25)  # Filtro con 25 valores
         return cls._instance
 
     def get_prev_view(self):
-        """Returns the current preview view, if available."""
-        return self._prev_view
+        """Returns the current previous view, if available."""
+        prev_preview = self._prev_view[-1] if self._prev_view else None
+        self._prev_view.pop()
+        return prev_preview
 
     def set_prev_view(self, view):
-        """Assigns the current preview view."""
-        self._prev_view = view
+        """Assigns the current previous view."""
+        self._prev_view.append(view)
 
         
