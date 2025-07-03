f16f96cf369097946934816ab78bf7676ebe09d8
giocerz <grce2000@gmail.com>
2025-07-03 01:53:17 -0500

    fix: refactor FolderSectionView to streamline UI components and improve scroll functionality

--- src/views/SaveDataView/FolderSectionView.py
+++ src/views/SaveDataView/FolderSectionView.py
@@ -1,16 +1,15 @@
 from PySide2.QtWidgets import QMainWindow, QStackedLayout, QVBoxLayout, QWidget, QGridLayout, QSizePolicy
-from PySide2.QtCore import Qt
+from PySide2.QtCore import Qt, QSize
+from PySide2.QtGui import QIcon
 from .ui.ui_FolderSection import Ui_MainWindow
 from src.widgets.KeyboardWidget import KeyboardWidget
-from src.widgets.PopupWidget import PopupWidgetInfo, PopupWidget
+from src.widgets.PopupWidget import PopupWidgetInfo
 from src.widgets.FolderWidget import FolderWidget
-from src.model.SensorData import SensorData
 from src.package.Navigator import Navigator
 from src.providers.SaveProvider import SaveProvider
-from src.model.Models import LoteModel, WaterQualityParams
+from src.model.Models import LoteModel
 from src.model.WaterQualityDB import WaterDataBase
-from src.config import Constants
-from datetime import datetime
+
 
 class FolderSectionView(QMainWindow):
     def __init__(self, context):
@@ -33,51 +32,20 @@ class FolderSectionView(QMainWindow):
         
         self.ui.verticalSlider.valueChanged.connect(self.slider_value_changed)
 
-        self.scrollBar = self.ui.scrollArea.verticalScrollBar()
         self.scrollBar.rangeChanged.connect(self.adjust_slider_range)
         self.scrollBar.valueChanged.connect(self.scroll_value_changed)
 
     def ui_components(self):
-        self.ui.stackedWidget.setCurrentIndex(self.current_index)
-        self.keyboard = KeyboardWidget(self.ui.inputPlace)
-        layout = QStackedLayout(self.ui.widgetKeyboard)
-        layout.addWidget(self.keyboard)
-        self.ui.widgetKeyboard.setLayout(layout)
+        icon = QIcon('./src/resources/icons/back.png')
+        self.ui.backBtn.setIcon(icon)
+        self.ui.backBtn.setIconSize(QSize(30, 30))
+        self.scrollBar = self.ui.scrollArea.verticalScrollBar()
+        self.ui.verticalSlider.setRange(self.scrollBar.minimum(), self.scrollBar.maximum())
         self.ui.verticalSlider.hide()
-    
-    def on_back_clicked(self):
-        if self.current_index == 1 and not self.folder_list_empty:
-            self.current_index = 0
-            self.ui.stackedWidget.setCurrentIndex(self.current_index)
-        else:
-            prev_view = self.save_provider.get_prev_view()
-            Navigator.pushReplacement(context=self.context, view=prev_view(context=self.context))
-        
-    def on_create_folder_clicked(self):
-        if self.current_index == 0:
-            self.current_index = 1
-            self.ui.stackedWidget.setCurrentIndex(self.current_index)
-
-    def show_dialog_error(self, error: str):
-        dialog = PopupWidgetInfo(context=self.context, text=error)
-        dialog.show()
 
     def load_data(self):
         self.folders_list = WaterDataBase.get_lotes()
-
-    def on_push_folder_widget(self, folder_widget: FolderWidget):
-        self.folder_id = folder_widget.id
-        self.folder_name = folder_widget.name
-
-        # Oculta el "label de selección" del anterior si existe
-        if self.selected_folder_widget and self.selected_folder_widget != folder_widget:
-            self.selected_folder_widget.set_selected(False)
-
-        folder_widget.set_selected(True)
-
-        # Guardar el nuevo seleccionado
-        self.selected_folder_widget = folder_widget
-
+    
     def setup_list(self):
         self.load_data()
 
@@ -85,13 +53,14 @@ class FolderSectionView(QMainWindow):
             self.current_index = 1
             self.ui.stackedWidget.setCurrentIndex(self.current_index)
             self.folder_list_empty = True
+            self.ui.verticalSlider.hide()
+            self.ui.scrollArea.hide()
             return
 
         # Crear el contenedor principal y el layout vertical
         container_widget = QWidget()
         main_layout = QVBoxLayout(container_widget)
-        # Alinear contenido en la parte superior
-        main_layout.setAlignment(Qt.AlignTop)
+        main_layout.setAlignment(Qt.AlignTop)  # Alinear contenido en la parte superior
         main_layout.setSpacing(10)
 
         # Crear el layout en cuadrícula
@@ -99,10 +68,9 @@ class FolderSectionView(QMainWindow):
         grid_layout.setSpacing(10)
 
         num_cols = 3  # Número de columnas
-        for i, product in enumerate(self.folders_list):
+        for i, folder in enumerate(self.folders_list):
             # Crear y configurar el widget
-            product_widget = FolderWidget(
-                id=product.id, name=product.name, description=product.description, on_push=self.on_push_folder_widget, type="select")
+            product_widget = FolderWidget(id=folder.id, name=folder.name, description=folder.description, on_push=self.on_push_folder_widget)
             product_widget.setFixedSize(133, 100)  # Fijar tamaño del widget
             product_widget.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
 
@@ -124,16 +92,14 @@ class FolderSectionView(QMainWindow):
         # Ajustar el contenedor dentro del ScrollArea
         self.ui.scrollArea.setWidget(container_widget)
         self.ui.scrollArea.setWidgetResizable(True)
-        container_widget.updateGeometry()
-        self.ui.scrollArea.updateGeometry()
-        self.updateGeometry()
+        self.ui.uploadBtn.show()
+
 
     def slider_value_changed(self, value):
         self.scrollBar.setValue(value)
 
     def adjust_slider_range(self, minValue, maxValue): 
         if maxValue > minValue:  # Solo mostramos el slider si hay algo que desplazar
-            print('Adjusting slider range:', minValue, maxValue)
             self.ui.verticalSlider.show()
             self.ui.verticalSlider.setRange(minValue, maxValue)
             
@@ -151,15 +117,53 @@ class FolderSectionView(QMainWindow):
         else:
             self.ui.verticalSlider.hide()
 
-
     def scroll_value_changed(self, value):
         self.ui.verticalSlider.setValue(value)
+    
 
+
+    
+    
+    def on_back_clicked(self):
+        if self.current_index == 1 and not self.folder_list_empty:
+            self.current_index = 0
+            self.ui.stackedWidget.setCurrentIndex(self.current_index)
+        else:
+            prev_view = self.save_provider.get_prev_view()
+            Navigator.pushReplacement(context=self.context, view=prev_view(context=self.context))
+        
+    def on_create_folder_clicked(self):
+        if self.current_index == 0:
+            self.current_index = 1
+            self.ui.stackedWidget.setCurrentIndex(self.current_index)
+
+    def show_dialog_error(self, error: str):
+        dialog = PopupWidgetInfo(context=self.context, text=error)
+        dialog.show()
+
+    """
+    def on_push_folder_widget(self, folder_widget: FolderWidget):
+        self.folder_id = folder_widget.id
+        self.folder_name = folder_widget.name
+
+        # Oculta el "label de selección" del anterior si existe
+        if self.selected_folder_widget and self.selected_folder_widget != folder_widget:
+            self.selected_folder_widget.set_selected(False)
+
+        folder_widget.set_selected(True)
+
+        # Guardar el nuevo seleccionado
+        self.selected_folder_widget = folder_widget
+    """
+    
+
+    """
     def validator(self) -> bool:
         if (not self.folder_name):
             self.show_dialog_error(error='Seleccione una carpeta')
             return False
 
+   
     #Verify if a lote exist
     def verify_repeated_lote_name(self, name:str):
         name_lw = name.strip().lower()
@@ -169,55 +173,4 @@ class FolderSectionView(QMainWindow):
             if is_repeated:
                 break
         return is_repeated
-    
     """
-    def on_save_clicked(self):        
-        place = self.ui.inputPlace.text().strip()
-
-        dtatetime_now = datetime.now()
-        format_date = dtatetime_now.strftime("%Y-%m-%d")
-        hour = dtatetime_now.strftime("%H:%M:%S")
-
-        if self.folder_id:
-            lote_id = self.folder_id
-        else:
-            lote = LoteModel(
-                name=self.folder_name,
-                creation_date=format_date,
-                creation_hour=str(hour),
-                last_add_date=format_date,
-                last_add_hour=str(hour),
-            )
-            lote_id = WaterDataBase.insert_lote(lote)
-
-        counter = 0
-        for sample in self.capture_samples:
-            sample_name = place if counter == 0 else f"{place} {counter}"
-            params = WaterQualityParams(
-                name=sample_name,
-                device_id=Constants.DEVICE_ID,
-                latitude=self.latitude,
-                longitude=self.longitude,
-                date=format_date,
-                hour=str(hour),
-                sample_origin=self.origin,
-                it_rained=self.it_rained,
-                upload_state=0,
-                lote_id=lote_id,
-                conductivity=sample.conductivity,
-                oxygen=sample.oxygen,
-                ph=sample.ph,
-                temperature=sample.temperature,
-                tds=sample.tds,
-                turbidity=sample.turbidity,
-                battery=sample.battery
-            )
-            WaterDataBase.insert_water_param(params)
-            counter += 1
-
-        message = "La muestra se guardó exitosamente" if len(self.capture_samples) == 1 else "Las muestras se guardaron exitosamente"
-        self.close_monitoring_callback()
-        finish_popup = PopupWidgetInfo(
-            context=self.context, text=message, on_click=self.close_view, is_warning=False)
-        finish_popup.show()
-        """
